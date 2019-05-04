582e537beb5d99f6dfb8a3cbc8637623071ccc47
Frittenfett <ruv_F@web.de>
2019-05-04 13:07:22 +0200

    Init

---
+++ README.md
@@ -0,0 +1,14 @@
+# streamlabs_songlist
+
+### this scripts adds a songlist to your streamlabs chatbot.
+The available default commands are:
+* !songlist
+
+The plugin does have a full UI Setting support and is full customable.
+
+Have fun :)
+
+
+### Changelog
+
+* v1.0.0 Major release
---
+++ songlist_StreamlabsSystem.py
@@ -0,0 +1,77 @@
+# ---------------------------------------
+#   Import Libraries
+# ---------------------------------------
+import json
+import codecs
+import os
+import clr
+
+clr.AddReference("IronPython.Modules.dll")
+import urllib
+
+# ---------------------------------------
+#   [Required]  Script Information
+# ---------------------------------------
+ScriptName = "Songlist"
+Website = "https://www.twitch.tv/frittenfettsenpai"
+Description = "A songlist for your stream."
+Creator = "frittenfettsenpai"
+Version = "1.0.0"
+
+
+# ---------------------------------------
+#   [Required] Intialize Data (Only called on Load)
+# ---------------------------------------
+def Init():
+    global settings
+    settingsfile = os.path.join(os.path.dirname(__file__), "settings.json")
+
+    try:
+        with codecs.open(settingsfile, encoding="utf-8-sig", mode="r") as f:
+            settings = json.load(f, encoding="utf-8")
+    except:
+        settings = {
+            "command": "!songlist",
+            "defaultAmount": "10",
+            "maxAmount": "15",
+        }
+    return
+
+
+# ---------------------------------------
+#   [Required] Execute Data / Process Messages
+# ---------------------------------------
+def Execute(data):
+    global settings
+    if data.IsChatMessage():
+        user = data.User
+
+        if (data.GetParam(0).lower() == settings["command"]):
+            if data.GetParamCount() > 1:
+                limit = int(data.GetParam(1))
+            else:
+                limit = int(settings["defaultAmount"])
+
+            if limit < 0 or limit > int(settings["maxAmount"]):
+                username = Parent.GetDisplayName(user)
+                Parent.SendTwitchMessage("{0} Limit has to be between 1 and {1}!".format(username, settings["defaultAmount"]))
+                return
+
+            songs = Parent.GetSongQueue(limit)
+            songList = ""
+            count = 1
+            for obj in songs:
+                if count > 1:
+                    songList = songList + " | "
+                songList = songList +str(count)+": "+songList + obj.Title
+                count = count + 1
+
+            Parent.SendTwitchMessage(songList)
+    return
+
+
+# ---------------------------------------
+#    [Required] Tick Function
+# ---------------------------------------
+def Tick():
+    return
